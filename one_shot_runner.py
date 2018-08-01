e1c48a894ebacd3037749e8245447882082b21c2
Alberto Arias <alberto@boodland.com>
2018-08-01 17:14:09 +0200

    Add test one shot logic

--- one_shot_runner.py
+++ one_shot_runner.py
@@ -26,6 +26,18 @@ class OneShotRunner():
             
         return [np.array(left_encoder_input), np.array(rigth_encoder_input)], labels
 
+    def __test_one_shot(self, num_ways=20, num_validation=250):
+        accuracy = 0
+        for _ in range(num_validation):
+            model_input, labels = self.__get_one_shot_batch(num_ways)
+            labels_hat = self.model.predict_on_batch(model_input)
+            correct = np.argmax(labels_hat)==np.argmax(labels)
+            accuracy += int(correct)
+
+        accuracy /= num_validation
+    
+        return accuracy*100.
+
     def __get_one_shot_batch(self, batch_size=20):
         left_encoder_input = []
         rigth_encoder_input = []
@@ -34,15 +46,15 @@ class OneShotRunner():
         classes_to_sample = self.dataset.get_data_classes(batch_size, data_type='test')
         true_class = classes_to_sample[0]
 
-        first_image, second_image = self.dataset.get_image_pair(true_class, same_class=True)
+        first_image, second_image = self.dataset.get_image_pair(true_class, same_class=True, data_type='test')
         left_encoder_input.append(first_image)
         rigth_encoder_input.append(second_image)
         labels[0] = 1
 
         for class_value in classes_to_sample[1:]:
-            _, second_image = self.dataset.get_image_pair(class_value)
+            _, second_image = self.dataset.get_image_pair(class_value, data_type='test')
             left_encoder_input.append(first_image)
             rigth_encoder_input.append(second_image)
 
         left_encoder_input, rigth_encoder_input, labels = shuffle(left_encoder_input, rigth_encoder_input, labels)
-        return np.array(left_encoder_input), np.array(rigth_encoder_input), labels
+        return [np.array(left_encoder_input), np.array(rigth_encoder_input)], labels
