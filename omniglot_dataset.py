2015a6578356d9493c602e7e4d37669ecb4785ae
Alberto Arias <alberto@boodland.com>
2018-08-02 16:19:09 +0200

    Split train data in train and val data Add get data type logic

--- omniglot_dataset.py
+++ omniglot_dataset.py
@@ -13,22 +13,26 @@ class OmniglotDataset:
         self.__data_loader = OmniglotLoader(self.__path, self.__train_folder, self.__test_folder)
         
         self.train_data = ()
+        self.val_data = ()
         self.test_data = ()
         self.data_shape = ()
     
     def load(self):
-        self.train_data, self.test_data = self.__data_loader.load_data()
-        _, _, height, width, channel = self.train_data[0].shape
+        train_val_data, self.test_data = self.__data_loader.load_data()
+        _, _, height, width, channel = train_val_data[0].shape
+        start_index_val = train_val_data[1].index('Latin/character01')
+        self.train_data = (train_val_data[0][:start_index_val], train_val_data[1][:start_index_val])
+        self.val_data =  (train_val_data[0][start_index_val:], train_val_data[1][start_index_val:])
         self.data_shape = (height, width, channel)
 
     def get_data_classes(self, num_classes, data_type='train'):
-        data = self.train_data[0] if data_type=='train' else self.test_data[0]
+        data = self.__get_data_type(data_type)
         classes = np.random.choice(data.shape[0], size=(num_classes,), replace=False)
         
         return classes
 
     def get_image_pair(self, class_value, data_type='train', same_class=False):
-        data = self.train_data[0] if data_type=='train' else self.test_data[0]
+        data = self.__get_data_type(data_type)
         num_classes, num_images, _, _, _ = data.shape
         image_indices = np.random.choice(num_images, size=2, replace=(not same_class))
         
@@ -45,3 +49,12 @@ class OmniglotDataset:
         different_value = np.random.choice(values)
         
         return different_value
+
+    def __get_data_type(self, data_type):
+        if (data_type == 'train'): 
+            return self.train_data[0]
+        elif (data_type == 'val'): 
+            return self.val_data[0]
+        else:
+            return self.test_data[0]
+
