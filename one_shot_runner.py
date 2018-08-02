30b012fccc02edfed987b4e2ce83670bad4ce558
Alberto Arias <alberto@boodland.com>
2018-08-02 11:21:43 +0200

    Add logic to store, retrieve runner data

--- one_shot_runner.py
+++ one_shot_runner.py
@@ -1,33 +1,71 @@
 from pathlib import Path
 import numpy as np
 from sklearn.utils import shuffle
+import pickle
 
 class OneShotRunner():
-    def __init__(self, dataset, model):
+    def __init__(self, dataset, model, preload_data=True):
         self.dataset = dataset
         self.model = model
 
-        self.__number_ways = 20
+        self.__number_ways = 20 
+        
+        self.__path = Path('./model/')
+        if not self.__path.exists():
+            self.__path.mkdir(parents=True, exist_ok=True)
+
+        self.__best_accuracy_weights_file = self.__path.joinpath('best_accuracy_weights.h5')
+        self.__training_data_file = self.__path.joinpath('training_data.pickle')
+        
+        self.__training_loss = []
+        self.__training_accuracy = []
+        
+        self.__best_accuracy = 0
+        self.__evaluate_every = 1000
+        self.__loss_every = 100
+        self.__store_every = 1000
+
+        if preload_data:
+            self.__preload_data()
+
+    def __preload_data(self):
+        print('Preloading runner data')
+        if self.__best_accuracy_weights_file.exists():
+            self.model.load_weights(str(self.__best_accuracy_weights_file))
+        
+        if self.__training_data_file.exists():
+            self.__read_data()
     
     def train(self, number_iterations=10000, num_validation=500):
-        test_every = 1000 # interval for evaluating on one-shot tasks
-        loss_every=100 # interval for printing loss (iterations)
-        #weights_path = os.path.join(PATH, "weights")
-        print("training")
-        for i in range(number_iterations):
+        print(f'Start training for {number_iterations} iterations with {num_validation} validations per evaluation')
+        for iteration in range(1, number_iterations):
             model_input, labels = self.__get_train_batch()
             loss = self.model.train_on_batch(model_input, labels)
 
-            if i % loss_every == 0:
-                print("iteration {}, training loss: {:.2f},".format(i,loss))
+            if iteration % self.__loss_every == 0:
+                self.__training_loss.append(loss)
+                print(f'iteration {iteration}, training loss: {loss:.2f}')
 
-            if i % test_every == 0:
-                print("evaluating")
-                accuracy = self.__test_one_shot(self.__number_ways, num_validation)
-                print(f'Accuracy at iteration {i} = {accuracy}')
+            if iteration % self.__evaluate_every == 0:
+                accuracy = self.__evaluate(iteration, num_validation)
+                self.__training_accuracy.append(accuracy)
+                print(f'Accuracy at iteration {iteration} = {accuracy}')
+
+            if iteration % self.__store_every == 0:
+                self.__save_data()
+                print(f'Saving training data at iteration {iteration}')
         
-        print("training loss after training: {:.2f},".format(loss))
+        print(f'Data after training: loss = {loss}, best accuracy {self.__best_accuracy:.2f}')
         
+    def __evaluate(self, iteration, num_validation):
+        accuracy = self.__test_one_shot(self.__number_ways, num_validation)
+        if accuracy > self.__best_accuracy:
+            print(f'Saving model weights with best accuracy of {accuracy:.2f}')
+            self.model.save_weights(self.__best_accuracy_weights_file)
+            self.__best_accuracy = accuracy
+
+        return accuracy
+
     def __get_train_batch(self, batch_size=32):       
         half_batch_size = batch_size // 2
         left_encoder_input = []
@@ -76,3 +114,13 @@ class OneShotRunner():
 
         left_encoder_input, rigth_encoder_input, labels = shuffle(left_encoder_input, rigth_encoder_input, labels)
         return [np.array(left_encoder_input), np.array(rigth_encoder_input)], labels
+
+    def __save_data(self):
+        data = self.__training_loss, self.__training_accuracy, self.__best_accuracy
+        with open(str(self.__training_data_file), "wb") as f:
+	        pickle.dump(data, f)
+
+    def __read_data(self):
+        with open(str(self.__training_data_file), "rb") as f:
+            data = pickle.load(f)
+        self.__training_loss, self.__training_accuracy, self.__best_accuracy = data
