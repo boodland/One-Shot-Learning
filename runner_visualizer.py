952103b97cdaf3a635fb20b85197d1295ef38ce4
Alberto Arias <alberto@boodland.com>
2018-08-03 15:12:58 +0200

    Add logic to visualize training accuracy and predictions

--- runner_visualizer.py
+++ runner_visualizer.py
@@ -30,3 +30,42 @@ class RunnerVisualizer:
             x, y = xy
             ax.text(x-8, (np.log(y)+3)/10, f'{y:.2f}', fontsize=15)
         plt.show()
+
+    @staticmethod
+    def display_accuracy(training_accuracy):
+        x = []
+        y = []
+        for i in range(0, len(training_accuracy), 10):
+            epoch = training_accuracy[i:i+10]
+            x.append(np.argmax(epoch)+i)
+            y.append(np.max(epoch))
+
+        sns.set_style("white")
+        plt.figure(figsize=(14,5))
+        plt.plot(training_accuracy)
+        plt.plot(x, y, 'o')
+        plt.ylim(60, 95)
+        plt.tick_params(axis='both', labelsize=14)
+        ax = plt.gca()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        for xy in zip(x, y):
+            x, y = xy
+            ax.text(x-1.2, y+1.2, f'{y:.2f}', fontsize=15)
+        plt.show()
+
+    @staticmethod
+    def display_predictions_accuracy(predictions):
+        sns.set_style("whitegrid")
+        plt.figure(figsize=(10,5))
+        ax = sns.boxplot(data= predictions, orient='v')
+        ax.xaxis.set_ticklabels(['Train', 'Validation', 'Test'])
+        ax.xaxis.set_ticks_position('none')
+        ax.spines['top'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        plt.tick_params(axis='y', labelsize=15)
+        plt.tick_params(axis='x', labelsize=20)
+        plt.ylim(64, 101)
+        plt.show()
