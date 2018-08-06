ccb3fb277270fd578564c568dae485c1122364fb
Alberto Arias <alberto@boodland.com>
2018-08-07 00:24:50 +0200

    Big refactoring

--- one_shot_runner.py
+++ one_shot_runner.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import numpy as np
 from sklearn.utils import shuffle
+from keras import backend as K
+
 from utils import Utils
 
 class OneShotRunner:
 
-    def __init__(self, dataset, model, preload_data=False):
+    def __init__(self, dataset, model):
         self.dataset = dataset
         self.model = model
         
@@ -13,77 +15,76 @@ class OneShotRunner:
         if not self.__path.exists():
             self.__path.mkdir(parents=True, exist_ok=True)
 
-        self.__best_accuracy_weights_file = self.__path.joinpath('best_accuracy_weights.h5')
+        self.__model_weights_file = self.__path.joinpath('model_weights.h5')
         self.__training_data_file = self.__path.joinpath('training_data.pickle')
         self.__predictions_data_file = self.__path.joinpath('predictions_data.pickle')
         
         self.__training_loss = []
         self.__training_accuracy = []
-        
-        self.__best_accuracy = 0
-        self.__evaluate_every = 1000
-        self.__loss_every = 100
-        self.__store_every = 1000
 
-        if preload_data:
-            self.__preload_data()
+        self.__validation_loss = []
+        self.__validation_accuracy = []
+        
+        self.__report_every = 100
+        self.__evaluate_every = 1000
+        self.__save_every = 1000
+
+    def __preload_weights(self):
+        if self.__model_weights_file.exists():
+            print('Preloading model weights')
+            self.model.load_weights(str(self.__model_weights_file))
 
     def __preload_data(self):
-        if self.__best_accuracy_weights_file.exists():
-            print('Preloading best accuracy model weights')
-            self.model.load_weights(str(self.__best_accuracy_weights_file))
-        
         if self.__training_data_file.exists():
             print('Preloading training data')
             data = Utils.read_data(str(self.__training_data_file))
-            self.__training_loss, self.__training_accuracy, self.__best_accuracy = data
+            self.__training_loss, self.__training_accuracy, self.__validation_loss, self.__validation_accuracy = data
     
-    def train(self, number_ways=20, number_iterations=10000, number_validations=50):
-        print(f'Start training for {number_iterations} iterations with {number_validations} validations per each {number_ways}-ways evaluation')
+    def train(self, number_ways=20, number_iterations=10000, number_validations=50, preload_state=False):
+        if preload_state:
+            self.__preload_weights()
+            self.__preload_data()
+
+        print(f'Start training for {number_iterations} iterations with {number_validations} validations per {number_ways}-ways evaluation')
         for iteration in range(1, number_iterations+1):
             model_input, labels = self.__get_train_batch()
-            loss = self.model.train_on_batch(model_input, labels)
-
-            if iteration % self.__loss_every == 0:
+            loss, accuracy = self.model.train_on_batch(model_input, labels)
+            accuracy *= 100.
+            if iteration % self.__report_every == 0:
                 self.__training_loss.append(loss)
-                print(f'iteration {iteration}, loss = {loss:.2f}')
+                self.__training_accuracy.append(accuracy)
+                print(f'iteration {iteration}, loss = {loss:.2f}, accuracy = {accuracy:.2f}')
 
             if iteration % self.__evaluate_every == 0:
-                accuracy = self.__evaluate(number_ways, iteration, number_validations)
-                self.__training_accuracy.append(accuracy)
-                print(f'iteration {iteration}, accuracy = {accuracy:.2f}')
+                loss, accuracy = self.__evaluate_one_shot(number_ways, number_validations)
+                self.__validation_loss.append(loss)
+                self.__validation_accuracy.append(accuracy)
+                print(f'evaluation at iteration {iteration}, loss = {loss:.2f}, accuracy = {accuracy:.2f}')
 
-            if iteration % self.__store_every == 0:
+            if iteration % self.__save_every == 0:
                 print(f'Saving training data at iteration {iteration}')
-                data = self.__training_loss, self.__training_accuracy, self.__best_accuracy
+                data = self.__training_loss, self.__training_accuracy, self.__validation_loss, self.__validation_accuracy
                 Utils.save_data(str(self.__training_data_file), data)
+                self.model.save_weights(self.__model_weights_file)
         
-        print(f'Data after training: loss = {loss}, best accuracy = {self.__best_accuracy:.2f}')
-        
-    def predict(self, number_ways=20, number_iterations=100, number_validations=50):
-        print(f'Start predictions for {number_iterations} iterations with {number_validations} validations per each {number_ways}-ways prediction')
+    def predict(self, number_ways=20, number_iterations=100, number_validations=50, preload_weights=False):
+        if preload_weights:
+            self.__preload_weights()
+
+        print(f'Start predictions for {number_iterations} iterations with {number_validations} validations per {number_ways}-ways prediction')
         train_accuracy = []
         val_accuracy = []
         test_accuracy = []
         predict_every = 10
         for iteration in range(1, number_iterations+1):
             train_accuracy.append(self.__test_one_shot(number_ways, number_validations, data_type='train'))
-            val_accuracy.append(self.__test_one_shot(number_ways, number_validations))
-            test_accuracy.append(self.__test_one_shot(number_ways, number_validations, data_type='test'))
+            val_accuracy.append(self.__test_one_shot(number_ways, number_validations, data_type='val'))
+            test_accuracy.append(self.__test_one_shot(number_ways, number_validations))
             if iteration % predict_every == 0:
                 print(f'Predictions at iteration {iteration} finished')
         data = train_accuracy, val_accuracy, test_accuracy
         print(f'Saving prediction data to {self.__predictions_data_file}')
         Utils.save_data(str(self.__predictions_data_file), data)
-
-    def __evaluate(self, number_ways, iteration, number_validations):
-        accuracy = self.__test_one_shot(number_ways, number_validations)
-        if accuracy > self.__best_accuracy:
-            print(f'Saving model weights with best accuracy of {accuracy:.2f}')
-            self.model.save_weights(self.__best_accuracy_weights_file)
-            self.__best_accuracy = accuracy
-
-        return accuracy
 
     def __get_train_batch(self, batch_size=32):       
         half_batch_size = batch_size // 2
@@ -101,7 +102,23 @@ class OneShotRunner:
             
         return [np.array(left_encoder_input), np.array(rigth_encoder_input)], labels
 
-    def __test_one_shot(self, number_ways, number_validations, data_type='val'):
+    def __evaluate_one_shot(self, number_ways, number_validations, data_type='val'):
+        accuracy = 0
+        loss = 0
+        for _ in range(number_validations):
+            model_input, labels = self.__get_one_shot_batch(number_ways, data_type)
+            labels_hat = self.model.predict_on_batch(model_input)
+            y_true = K.variable(labels)
+            y_pred = K.variable(labels_hat.squeeze())
+            loss += K.eval(self.model.loss_functions[0](y_true, y_pred))
+            correct = np.argmax(labels_hat)==np.argmax(labels)
+            accuracy += int(correct)
+
+        accuracy /= number_validations
+        loss /= number_validations
+        return loss, accuracy*100.
+
+    def __test_one_shot(self, number_ways, number_validations, data_type='test'):
         accuracy = 0
         for _ in range(number_validations):
             model_input, labels = self.__get_one_shot_batch(number_ways, data_type)
@@ -110,7 +127,6 @@ class OneShotRunner:
             accuracy += int(correct)
 
         accuracy /= number_validations
-    
         return accuracy*100.
 
     def __get_one_shot_batch(self, batch_size, data_type):
